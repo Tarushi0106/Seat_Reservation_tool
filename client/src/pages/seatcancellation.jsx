89ff2b1347b8f58f145cd5a113534923b3bfab15
Tarushi Chaudhary <tarushi0106@gmail.com>
2025-03-24 16:25:02 +0530

    Add existing project files to Git

--- client/src/pages/seatcancellation.jsx
+++ client/src/pages/seatcancellation.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './seatcancel.css';
 
 const SeatCancellation = () => {
   const [seatnumber, setSeatnumber] = useState('');
